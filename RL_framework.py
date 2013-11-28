5cf3a680f0cf27c7e31e5573f185bc929f083527
Minh-Tue Vo <minhtuev@mit.edu>
2013-11-28 17:20:30 -0500

    adding actions and states

--- RL_framework.py
+++ RL_framework.py
@@ -9,7 +9,7 @@ class RLObject:
 			return False
 
 	def __ne__(self, other):
-		return not self.__eq__(other)
+		return not self.__eq__(other)  
 
 class Action(RLObject):
 	def __str__(self):
@@ -19,7 +19,32 @@ class State(RLObject):
 	def __str__(self):
 		return "State " + self.name
 
-# class Model: 
+class Model(RLObject):
+	def __init__(self, name):
+		self.name = name
+		# list of all actions
+		self.actions = []
+		# list of all states
+		self.states = []
+		self.current_state = None
+
+	def __str__(self):
+		return "Model:" + self.name 
+
+	def get_current_state(self):
+		raise Exception("not implemented")
+
+	# get all available actions for this state
+	def get_actions(self):
+		raise Exception("not implemented")
+
+	# get the available next states from this state - might
+	def get_next_states(self):
+		raise Exception("not implemented")
+
+	def perform_action(self, action):
+		raise Exception("not implemented")
+
 a = Action("1")
 b = Action("1")
 c = Action("2")
