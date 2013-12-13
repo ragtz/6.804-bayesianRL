037a3654fb7efdd8fda69bfbdd4ede90c496b5fb
Minh-Tue Vo <minhtuev@mit.edu>
2013-12-13 14:31:53 -0500

    adding book-keeper class & bayes dp

---
+++ BayesPrioritizedSweeping.py
@@ -0,0 +1,10 @@
+from PrioritizedSweeping import *
+
+# for a model, this class holds all the data
+class Hypothesis(object):
+	def __init__(self):
+		pass
+
+class BayesPrioritizedSweeping(PrioritizedSweeping):
+	def __init__(self):
+		pass
--- RL_framework.py
+++ RL_framework.py
@@ -67,7 +67,40 @@ class Model(RLObject):
     def reset(self):
         self.current_state  = self.start_state
 
+# this class keeps auxiliary information for RL algorithm
+class Keeper(object):
+    def __init__(self):
+        self.sum_reward_squares = {}
+        self.sum_reward = {}
+        self.visit_count_state = {}
+        self.visit_count_state_action = {}
+
+    def get_sum_reward(self, state, action):
+        return self.sum_reward.get((state, action), 0)
+
+    def update_sum_reward(self, state, action, r):
+        self.sum_reward[(state, action)] = self.get_sum_reward(state, action) + r
+
+    def get_sum_reward_squares(self, state, action):
+        return self.sum_reward_squares.get((state, action), 0)
+
+    def update_sum_reward_squares(self, state, action, r):
+        self.sum_reward_squares[(state, action)] = self.get_sum_reward_squares(state, action) + r**2
+
+    def get_visit_count(self, state):
+        return self.visit_count_state[state]
+
+    def get_visit_count(self, state, action):
+        return self.visit_count_state_action[(state, action)]
+
+    def increase_count(self, state, action):
+        self.visit_count_state[state] = self.get_visit_count(state) + 1
+        self.visit_count_state_action[(state, action)] = self.get_visit_count(state, action) + 1
+
 class RLAlgorithm(object):
+    def __init__(self):
+        self.keepr = Keeper()
+
     # compute transition function P(s1, a, s2)
     def get_transition(self, s1, a, s2):
         v = (s1, a, s2)
